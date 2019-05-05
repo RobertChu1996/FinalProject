3e2eb0d5e2c4ad42a54991870243cb00bd4cc30b
johnlee11263 <john.lee15@outlook.com>
2019-05-05 13:57:17 -0500

    Made it so Nav. Works with all buttons

--- dropdown.js
+++ dropdown.js
@@ -17,3 +17,57 @@ window.onclick = function(event){
     }
   }
 }
+
+function dropFunction2(){
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+//closes dropdown menu if clicked outside of it
+window.onclick = function(event){
+  if (!event.target.matches('.dropbtn')){
+    var dropdowns = document.getElementByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')){
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function dropFunction3(){
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+//closes dropdown menu if clicked outside of it
+window.onclick = function(event){
+  if (!event.target.matches('.dropbtn')){
+    var dropdowns = document.getElementByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')){
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function dropFunction4(){
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+
+//closes dropdown menu if clicked outside of it
+window.onclick = function(event){
+  if (!event.target.matches('.dropbtn')){
+    var dropdowns = document.getElementByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')){
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
